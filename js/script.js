5bce85f6da78de10e2b52bc8cd263fabd95680cf
YuraLinkev1ch <yura.linkevich2012@gmail.com>
2020-02-07 16:19:17 +0000

    added blog, about

--- js/script.js
+++ js/script.js
@@ -6,15 +6,11 @@ $(document).ready(function(){
         prevArrow:'<div class="slick-arrow-prev"><img  src="img/home/slider-arrow-previous.svg"></div>',
         nextArrow:'<div class="slick-arrow-next"><img src="img/home/slider-arrow-next.svg"></div>',
         asNavFor:'.big-slider',
-        focusOnSelect: true,
-        autoplay:true,
-        autoplaySpeed:3000,
-        speed:700,
+        
     });
 
     $('.big-slider').slick({
         arrows:false, 
         asNavFor:'.slider',
-        speed:700,
     })
 })
