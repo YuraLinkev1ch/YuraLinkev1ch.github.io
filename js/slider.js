2d6d76ccbdf9c60d63c9ccfce16fb76365464ca7
yuralinkev1ch <yura.linkevich2012@gmail.com>
2020-08-06 08:52:06 +0300

    add swiper

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 0,
+    slidesPerView: 6,
+    loop: false,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      786 : {
+        spaceBetween: 20,
+      }
+    }
+  });
+
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+    autoplay: {
+      delay: 4000,
+    },
+  });
